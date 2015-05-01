7baaaff5b583981b680ac1871330698cbabdbf96
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-05-01 12:58:03 -0400

    remove the -x option in fetch_tarball.sh

--- fetch_tarball.sh
+++ fetch_tarball.sh
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-set -ex
+set -e
 
 VERSION=$(cat VERSION)
 URL="https://github.com/cablehead/python-consul/archive/v${VERSION}.tar.gz"
